a957fe0d51fe56a4cb2e28f9be3858a4274d352a
suntzu93 <thanhlv93@gmail.com>
2022-11-01 05:56:39 +0700

    Fix sql

--- manage_actions.py
+++ manage_actions.py
@@ -157,7 +157,7 @@ def get_healthy_subgraph():
         subgraphs = request.form.get("subgraphs")
         if token == config.token:
             graphql_healthy_subgraph = """
-                            { indexingStatuses(subgraphs: [%s]) { subgraph synced health fatalError {message deterministic block { number }} }}}
+                            { indexingStatuses(subgraphs: [%s]) { subgraph synced health fatalError {message deterministic block { number }} }}
                             """ % subgraphs
             response = requests.post(url=config.indexer_node_rpc,
                                      json={"query": graphql_healthy_subgraph})
