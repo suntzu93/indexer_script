5e14dde543f756cdeb987c167e667ee8b40be516
suntzu93 <thanhlv93@gmail.com>
2022-11-18 08:50:49 +0700

    update link gist

--- install.sh
+++ install.sh
@@ -16,5 +16,5 @@ pip3 install flask
 pip3 install -U flask-cors
 
 #download config
-wget https://gist.githubusercontent.com/suntzu93/8692faff0a60fc8acc1c3e09daedd3a3/raw/19f4cf959b2a19edd499b8be00c170076bf4aa98/config.py
-wget https://gist.githubusercontent.com/suntzu93/8b3f1969e84125fdf6f02ea2902cb58e/raw/01016fc199a321425b7719c68aebe23b0caaf2ce/graphman_config.toml
+wget https://gist.githubusercontent.com/suntzu93/8692faff0a60fc8acc1c3e09daedd3a3/raw/config.py
+wget https://gist.githubusercontent.com/suntzu93/8b3f1969e84125fdf6f02ea2902cb58e/raw/graphman_config.toml
