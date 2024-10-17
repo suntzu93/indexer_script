085cfecfffcdffda4912d9b9348d40c6624e77ad
suntzu <thanhlv93@gmail.com>
2024-10-17 20:25:05 +0700

    fix API

--- const.py
+++ const.py
@@ -1,4 +1,4 @@
-HOST = "https://api.graphindexer.co/"
+HOST = "https://api.graphindexer.suntzu.dev"
 API_GET_CLOSE_ACTIONS = HOST + "/api/closeActions"
 API_GET_OPEN_ACTIONS = HOST + "/api/openActions"
 API_UPDATE_EXE_STATUS = HOST + "/api/updateExeStatus"
